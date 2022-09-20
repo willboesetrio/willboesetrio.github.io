0a9f57feb7734100da9ee161d762a08c94a79397
Will Boese <thewillboesetrio@gmail.com>
2022-09-19 21:29:21 -0400

    updated header footer and css

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
 console.log('testing');
-document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
-document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+const htmlBtn = document.getElementById("htmlBtn");
+htmlBtn.setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+const cssBtn = document.getElementById("cssBtn");
+cssBtn.setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
