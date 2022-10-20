e319c354bfb1d9cf3f9f02abcdeb5746be26e4d9
Will Boese <thewillboesetrio@gmail.com>
2022-10-20 11:18:58 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 
-const htmlBtn = document.getElementById("html-btn");
-htmlBtn.setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
-const cssBtn = document.getElementById("css-btn");
-cssBtn.setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+// const htmlBtn = document.getElementById("html-btn");
+// htmlBtn.setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+// const cssBtn = document.getElementById("css-btn");
+// cssBtn.setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+document
+      .getElementById("validation_link_html")
+      .setAttribute(
+        "href",
+        "https://validator.w3.org/check?uri=" + location.href
+      );
+    document
+      .getElementById("validation_link_css")
+      .setAttribute(
+        "href",
+        "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href
+      );
