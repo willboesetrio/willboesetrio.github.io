18cc77a90169cf8e7bbcad358773318f594403fe
Will Boese <thewillboesetrio@gmail.com>
2022-11-01 08:11:54 -0400

    added stretch folder

---
+++ assignments/stretch/script.js
@@ -0,0 +1,120 @@
+const teamMembers =  [
+    {
+        first_name: 'William',
+        last_name: 'Boese',
+        image_url: 'images/will_profile_min.jpeg',
+        image_caption: 'profile photo',
+        personal_background: 'raised in New Jersey, lived in the Carolinas since adulthood',
+        academic_background: 'Associates degree in fine arts',
+        background_in_this_subject: 'Attended NuCamp coding bootcamp',
+        primary_computer_platform: 'Windows',
+        courses_im_taking: ['CP-C0-01', 'Applied Music', 'Basket-Weaving'],
+        funny_interesting_item: 'I am a musician and composer.',
+        also_like_to_share: 'I also enjoy cooking and running.',
+    },
+    {
+        first_name: 'Keiona',
+        last_name: 'Leaks',
+        image_url: 'images/eyecandy.jpeg',
+        image_caption: 'Giving you some eye candy.',
+        personal_background: "I’m Keiona Leaks. I’m originally from Jacksonville, NC. I moved to Charlotte when I was 15. I have a sweet little puppy and two fishes. I’m in love with the gym and painting. I’m extremely creative and artistic. I intend to reach all of my goals even if it hurts.",
+        academic_background: "I’m still super young so I don’t have the greatest academic background. I graduated from Garinger High School in 2020. During this time, I learned multiple instruments and introduced myself to computers.",
+        background_in_this_subject: 'I have lots of experience with photoshop. I forced myself to learn so I could convince people I had a twin. I also learned game design in high school. The library allowed me to check out multiple books on Web and Graphic Design.',
+        primary_computer_platform: 'This new school Dell laptop, and an old Lenovo that shuts down when you look in its direction.',
+        courses_im_taking: ["CRO7000-01: CO I want a better future for myself. I want the set of skills this course will teach me. I can create things on my own outside of work. It gives me a chance to artistically express myself in a way I haven’t before."],
+        funny_interesting_item: 'I know how to play 7 instruments, I know how to roller skate, and my favorite superhero is either Spiderman or Nightwing.',
+        also_like_to_share: 'I’m super helpful, talk to me if you ever need help. I promise you the answer is always yes.',
+    },
+    {
+        first_name: 'Jazmarie',
+        last_name: 'Moncrieft',
+        image_url: 'images/jazmarie_selfie.png',
+        image_caption: 'Feeling blessed!',
+        personal_background: 'I am a wife and a mother of 3 boys ages 7 months, 3, and 12. My husband and I moved from up north about 12 years ago. After God, basketball is everything to my family. We love spending time with our boys watching movies and watching their basketball games.',
+        academic_background: "I have a bachelor's degree in nursing. Most of my nursing has been in the long-term care setting (nursing homes). I attended college in Pennsylvania. This is my first time back in school after almost 15 years.",
+        background_in_this_subject: "I've always been a tech person, I like technology. I enjoy being the person who teaches and troubleshoots computer and programming problems at work. My brother is a software engineer. After learning about what he does, I became interested in software engineering. I was advised to start with Python, so I did a self-taught python course.",
+        primary_computer_platform: 'I usually use my iPad for everything.',
+        courses_im_taking: ['CR07000-01: Introduction to computers and programing',],
+        funny_interesting_item: 'I am fluent in english and spanish.',
+        also_like_to_share: '',
+    },
+    {
+        first_name: 'Donald',
+        last_name: 'Moore',
+        image_url: 'images/donald-hiking.jpg',
+        image_caption: '"There is no success without a struggle" - Don Moore - 9/2/98',
+        personal_background: "I’m a married father with 2 kids that are now teenagers. We moved to Charlotte from Ohio 10 years ago. For the last 9 years I have served as Scoutmaster and Assistant Scoutmaster for my son’s Boy Scout troop. We spend most of our time exposing our kids to different activities and attending their track and XC meets.",
+        academic_background: 'High School diploma, some college courses',
+        background_in_this_subject: 'I started learning how to code when I joined the “Fast Track” program at IBG solutions, a Charlotte based tech company. After a year in the program it seemed to become non-existent, so I continued to teach myself. Software is something that I watched change the world and has become very inmportant and incredibly useful in all of our lives. I would like to be a part of the force that continues to create software that will help people push their personal boudaries further by solving problems. I use and/or am familiar with JavaScript, HTML, CSS, Reactjs, Nextjs, React Native, Tailwind CSS, MUI, Git, Github, Bootstrap, MongoDB, SQL, and Firebase.',
+        primary_computer_platform: 'Mac OS',
+        courses_im_taking: ["CRO7000-01: I’m taking this course to help me refine my skills and cover anything I have missed in past training. More importantly, being in the paid apprenticeship program after completing the training helps me get over the hurdle of entering this industry with no on-the-job experience.",],
+        funny_interesting_item: "I’m left handed in sports, but I write and draw with my right hand. I cut my own hair. Can you tell?",
+        also_like_to_share: 'I have 2 pet leopard geckos.',
+    },
+    {
+        first_name: 'Richy',
+        last_name: 'Phongsavath',
+        image_url: 'images/rainbowmount-richy.jpg',
+        image_caption: 'Rainbow Mountain, Peru',
+        personal_background: 'I recently moved back to Charlotte, NC from Columbia, SC. I went to the University of South Carolina for my undergrad and postgrad degrees. I used to work in the nonprofit sector in the finance field for my organization, but I always had an interest in software development.',
+        academic_background: 'I have my Bachelors in Science - Biological Science and a Masters in Science - Health Information Technology.',
+        background_in_this_subject: 'I have completed an Intro to Programing class in undergrad that included HTML and CSS. I have used applications such as Microsoft Word, WordPress, and MySpace.',
+        primary_computer_platform: 'Windows 10',
+        courses_im_taking: ["CR07000-01: CO Programming - I’m taking the course as a prerequisite for the Create Opportunity program.",],
+        funny_interesting_item: 'I used to be a volunteer firefighter for the City of Columbia and I would teach yoga to the recruit classes.',
+        also_like_to_share: 'I recently gained an appreciation for nature and would like to visit all of the National Parks in the US. Any suggestions are greatly appreciated.',
+    },
+    {
+        first_name: 'Lucas',
+        last_name: 'Prieto',
+        image_url: 'images/mona.jpg',
+        image_caption: 'My dog, Mona',
+        personal_background: 'I have been living in North Carolina since 2009, and moved to Charlotte in 2019.',
+        academic_background: 'I graduated from North Mecklenburg High in 2019 and went on to enjoy my first semester of college at Daytona Beach until 2020 happened.',
+        background_in_this_subject: 'I had taken some low level high school courses on programming and was studying software engineering in college.',
+        primary_computer_platform: 'My desk, but sometimes I use the dining table. (Windows)',
+        courses_im_taking: ['CRO7000 - Create Opportunity Programming: I am taking this course because I am interested in its direct and career focused approach.'],
+        funny_interesting_item: 'I built a chicken coop for my mother when we moved to Charlotte, and it houses three hens.',
+        also_like_to_share: 'I am an Eagle Scout as of 2019.',
+    },
+    {
+        first_name: 'Churina',
+        last_name: 'Sa',
+        image_url: 'images/churina.jpg',
+        image_caption: 'Disney World, August 2022',
+        personal_background: 'I am a Mongolian from China. I moved to the US 3 years ago. I have a daughter who is 13 years old. My husband is a Hvac tech. We have a lovely cat named Mumu.',
+        academic_background: 'I got my phd degree in 2009. After that I worked as a professor in a Chinese University. When I moved to the US, I noticed I needed to change my career. So being a software engineer is my goal in this country!',
+        background_in_this_subject: "I don't have much experience in this field. But I self learned some basic coding from Youtube and datacamp.",
+        primary_computer_platform: "I use Windows at home. I used to try Mac before. But I can't get used to the system.",
+        courses_im_taking: ['CRO7000-01: CO Programming', 'CS50: Harvard Open Course',],
+        funny_interesting_item: 'I used to be a vegetarian, but my husband keeps saying "you have to eat meat because meat can provide you with necessary nutrients". As a result, I am eating some chicken and fish now.',
+        also_like_to_share: 'I speak Chinese, Mongolian and English.',
+    },
+    {
+        first_name: 'Tony',
+        last_name: 'Vanderhall',
+        image_url: 'images/photo-of-tony.jpg',
+        image_caption: 'Doing something in IT',
+        personal_background: 'I program and create music in my free time. I also work part-time at Ross.',
+        academic_background: 'I was an early graduate from Independence High School in Charlotte, NC.',
+        background_in_this_subject: "I've been programming since 6th grade learning various different programming languages such as: Golang, Python, C++, Lua, HTML & CSS, and a small amount of Rust. I have 350 points so far on freeCodeCamp",
+        primary_computer_platform: " I use windows mainly though we have a couple raspberry pi's at home.",
+        courses_im_taking: ['CRO7000-01: CO Programming : To become a full stack developer.',],
+        funny_interesting_item: "I'm really good at problem solving which is why I started programming in the first place.",
+        also_like_to_share: '',
+    },
+];
+const teamMembersList = document.getElementById('team-members-list');
+let testArray = [];
+for (let i = 0; i < teamMembers.length; i++) {
+    let currentStudentId = `${teamMembers[i].first_name}${teamMembers[i].last_name}`
+    teamMembersList.innerHTML += `<li id='${currentStudentId}'>${teamMembers[i].first_name} ${teamMembers[i].last_name}</li>`;
+    let currentElement = document.getElementById(currentStudentId);
+    testArray.push(currentElement);
+}
+for(let i = 0; i < testArray.length; i++) {
+    document.getElementById(testArray[i].id).addEventListener('click', () => {
+        console.log('test');
+        document.getElementById('team-member-introduction').innerHTML = `<h3>${teamMembers[i].first_name} ${teamMembers[i].last_name}</h3><figure><img src='${teamMembers[i].image_url}'><figcaption>${teamMembers[i].image_caption}</figcaption></figure><p><strong>Personal Background:</strong> ${teamMembers[i].personal_background}</p><p><strong>Academic Background:</strong> ${teamMembers[i].academic_background}</p><p><strong>Background in This Subject:</strong>${teamMembers[i].background_in_this_subject}</p><p><strong>Primary Computer Platform:</strong>${teamMembers[i].primary_computer_platform}</p>`;
+    })   
+}
