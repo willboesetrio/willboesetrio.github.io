87d6b1a21aae0c60e92721cbe5577060bf7f0575
Will Boese <thewillboesetrio@gmail.com>
2022-11-03 16:40:39 -0400

    updated assignments

--- assignments/to-do-list/script.js
+++ assignments/to-do-list/script.js
@@ -1,10 +1,20 @@
 const listItems = document.querySelectorAll('li');
 const addCompleted = (event) => {
     event.target.classList.toggle('completed')
+    console.log('click')
 }
 for (const li of listItems) {
     li.addEventListener('click', addCompleted)
 }
 const onAddToDo = (event) => {
-    const newListItem = 
+    const text = event.target.parentNode.querySelector('input').value;
+    if(text) {
+        const newListItem = document.createElement('li');
+        newListItem.addEventListener('click', addCompleted);
+        const liText = document.createTextNode(text);
+
+        newListItem.appendChild(liText);
+        document.querySelector('ul').appendChild(newListItem);
+    }
 }
+document.querySelector('button').addEventListener('click', onAddToDo)
